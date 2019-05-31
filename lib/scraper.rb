0b88cebe45939d9e7ba764d74106c31731399e5b
Joshua Crum <joshua.crum@gmail.com>
2019-05-31 17:46:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,7 +25,7 @@ require_relative './course.rb'
 class Scraper
 
   def get_page
-      # more code coming soon!
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 
 end
